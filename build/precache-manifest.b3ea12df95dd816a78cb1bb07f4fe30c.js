cbe017d058f8b497fe36fd62af13c152210ea19c
kiasNostieDev <akinthiyasrinath@gmail.com>
2020-09-05 15:00:49 +0000

    deploy mode

---
+++ build/precache-manifest.b3ea12df95dd816a78cb1bb07f4fe30c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbba84e74ce68626650878de64d382ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28e9ac564a18044f89db",
+    "url": "/static/css/main.cca44e13.chunk.css"
+  },
+  {
+    "revision": "089ffcb39fa44e9339fa",
+    "url": "/static/js/2.47942751.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/static/js/2.47942751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e9ac564a18044f89db",
+    "url": "/static/js/main.a3ba48bc.chunk.js"
+  },
+  {
+    "revision": "f9600b521b3de0d569e9",
+    "url": "/static/js/runtime-main.0a4ca0c9.js"
+  },
+  {
+    "revision": "e1057d32ef55babf01b1304936d419fb",
+    "url": "/static/media/antennna.e1057d32.jpg"
+  }
+]);
