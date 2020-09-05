dad74fe8edd8db3761fef849ab6b24513720b738
kiasNostieDev <akinthiyasrinath@gmail.com>
2020-09-05 16:47:02 +0000

    Updates

---
+++ precache-manifest.c74c8d96847e13688e9d527bb064ca1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d4dc2477b1265414fc78f680b55bf8",
+    "url": "/Antenna/index.html"
+  },
+  {
+    "revision": "bf24fd8e7deb6c7bacbf",
+    "url": "/Antenna/static/css/main.cca44e13.chunk.css"
+  },
+  {
+    "revision": "089ffcb39fa44e9339fa",
+    "url": "/Antenna/static/js/2.47942751.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/Antenna/static/js/2.47942751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf24fd8e7deb6c7bacbf",
+    "url": "/Antenna/static/js/main.a2148e22.chunk.js"
+  },
+  {
+    "revision": "a7f16d5928c8e1cf5694",
+    "url": "/Antenna/static/js/runtime-main.8b83667a.js"
+  },
+  {
+    "revision": "e1057d32ef55babf01b1304936d419fb",
+    "url": "/Antenna/static/media/antennna.e1057d32.jpg"
+  }
+]);
